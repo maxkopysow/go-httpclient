dd33af73aab1f8f7582422d16744ed01532f9bce
maxow <maxkopysow@gmail.com>
2024-04-17 19:07:04 +0300

    stable version 1

---
+++ gomime/headers.go
@@ -0,0 +1,9 @@
+package gomime
+
+const (
+	HeaderContentType      = "Content-Type"
+	HeaderUserAgent        = "User-Agent"
+	ContentTypeJson        = "application/json"
+	ContentTypeXML         = "application/xml"
+	ContentTypeOctetStream = "application/octet-stream"
+)
